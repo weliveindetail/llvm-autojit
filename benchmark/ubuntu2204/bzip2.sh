1c7be5db8b0c8dbebdbb3c1561f9d884211f2a02
Stefan Gränitz <stefan.graenitz@gmail.com>
2025-08-13 14:48:10 +0200

    Polish meantime.py with proper setup/teardown steps and mean (min/max) output

--- benchmark/ubuntu2204/bzip2.sh
+++ benchmark/ubuntu2204/bzip2.sh
@@ -6,23 +6,23 @@ mkdir bzip2
 
 # Regular bench
 echo "Building regular binary.."
-./timeavg.py --setup bzip2-regular-setup.sh --runs 20 bzip2-regular-build.sh
+./meantime.py --setup bzip2-regular-setup.sh --runs 50 bzip2-regular-build.sh
 
 # AutoJIT bench
 echo "Building AutoJIT binary.."
-./timeavg.py --setup bzip2-autojit-setup.sh --runs 20 bzip2-autojit-build.sh
+./meantime.py --setup bzip2-autojit-setup.sh --runs 50 bzip2-autojit-build.sh
 
 # Runtime bench
 rm -rf bzip2/outputs
 mkdir bzip2/outputs
 echo ""
 echo "Run-time regular:"
-./timeavg.py --runs 3 bzip2-regular-run.sh
+./meantime.py --runs 3 bzip2-regular-run.sh
 md5sum bzip2/outputs/data1_regular.txt
 md5sum bzip2/outputs/data2_regular.txt
 
 echo ""
 echo "Run-time autojit:"
-./timeavg.py --runs 3 bzip2-autojit-run.sh
+./meantime.py --runs 3 bzip2-autojit-run.sh
 md5sum bzip2/outputs/data1_autojit.txt
 md5sum bzip2/outputs/data2_autojit.txt
