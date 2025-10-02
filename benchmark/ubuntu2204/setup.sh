56698d01e9fd147217ea4af71fe80df73e36ca8a
Stefan Gränitz <stefan.graenitz@gmail.com>
2025-10-02 12:51:30 +0200

    Checkout llvm-project repo for FileCheck bench

--- benchmark/ubuntu2204/setup.sh
+++ benchmark/ubuntu2204/setup.sh
@@ -3,5 +3,10 @@ set -e
 
 if [ ! -d "llvm-autojit-bench" ]; then
     echo "Fetching benchmark code.."
-    git clone https://github.com/weliveindetail/llvm-autojit-bench llvm-autojit-bench
+    git clone https://github.com/weliveindetail/llvm-autojit-bench
+fi
+
+if [ ! -d "llvm-project" ]; then
+    echo "Fetching benchmark code.."
+    git clone https://github.com/llvm/llvm-project
 fi
