84a61b85773d82eb1fcfd0f9af4c354ed52b03a2
Stefan Gränitz <stefan.graenitz@gmail.com>
2025-10-20 10:44:58 +0200

    Split libcxx.cpp into dedicated tests for libc++ and libstdc++ (with daemon-mode)

---
+++ test/libstdcxx.cpp
@@ -0,0 +1,27 @@
+// RUN: %clang -fpass-plugin=%autojit_plugin -stdlib=libstdc++ -std=c++17 -c %s -o %t.o
+// RUN: %clang %t.o -rdynamic -lstdc++ -L%autojit_runtime_dir -Wl,-rpath=%autojit_runtime_dir -lautojit-runtime -o %t_inprocess.exe
+// RUN: %clang %t.o -rdynamic -lstdc++ -L%autojit_runtime_dir -Wl,--whole-archive -lautojit_static-%arch -Wl,--no-whole-archive -o %t_remote.exe
+// RUN: %t_inprocess.exe | FileCheck %s
+// RUN: %t_remote.exe | FileCheck %s
+
+// CHECK: No such file or directory;Is a directory;Invalid argument;Permission denied
+
+// This code is copied from a sample program that runs during the LLVM CMake
+// configuration process. It's one of the early candidates for failure when
+// building LLVM with autojit:
+// https://github.com/llvm/llvm-project/blob/release/20.x/llvm/cmake/modules/GetErrcMessages.cmake
+//
+#include <cerrno>
+#include <iostream>
+#include <string>
+#include <system_error>
+
+std::string getMessageFor(int err) {
+    return std::make_error_code(static_cast<std::errc>(err)).message();
+}
+
+int main() {
+    std::cout << getMessageFor(ENOENT) << ';' << getMessageFor(EISDIR);
+    std::cout << ';' << getMessageFor(EINVAL) << ';' << getMessageFor(EACCES);
+    return 0;
+}
